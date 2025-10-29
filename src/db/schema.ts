f0b57f6480c5eef6c1a09bba2ae5bbd0ed1aac23
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-10-29 15:42:31 -0700

    create presets schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,15 +1,14 @@
 import {
   pgTable,
-  // serial,
-  // integer,
   varchar,
-  // text,
-  // real,
-  // smallint,
+  text,
+  real,
+  smallint,
   timestamp,
   integer,
+  primaryKey,
 } from "drizzle-orm/pg-core"
-// import { relations } from "drizzle-orm"
+import { relations } from "drizzle-orm"
 
 export const users = pgTable("users", {
   id: integer("id").primaryKey().generatedAlwaysAsIdentity(),
@@ -18,96 +17,102 @@ export const users = pgTable("users", {
   createdAt: timestamp("created_at", { mode: "date" }).defaultNow().notNull(),
 })
 
-// export const usersRelations = relations(users, ({ many }) => ({
-//   presets: many(presets),
-// }))
+export const usersRelations = relations(users, ({ many }) => ({
+  presets: many(presets),
+}))
 
-// export const presets = pgTable("presets", {
-//   id: serial().primaryKey(),
-//   userId: integer()
-//     .notNull()
-//     .references(() => users.id, { onDelete: "cascade" }),
-//   name: varchar({ length: 100 }).notNull(),
-//   description: text(),
-//   createdAt: timestamp({ mode: "date" }).defaultNow().notNull(),
-//   updatedAt: timestamp({ mode: "date" })
-//     .defaultNow()
-//     .$onUpdate(() => new Date()),
-// })
+export const presets = pgTable("presets", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  name: varchar("name", { length: 100 }).notNull(),
+  description: text("description"),
+  createdAt: timestamp("created_at", { mode: "date" }).defaultNow().notNull(),
+  updatedAt: timestamp("updated_at", { mode: "date" })
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+})
 
-// export const presetsRelations = relations(presets, ({ one, many }) => ({
-//   user: one(users, {
-//     fields: [presets.userId],
-//     references: [users.id],
-//   }),
-//   oscillators: many(oscillators),
-//   tags: many(presetsToTags),
-// }))
+export const presetsRelations = relations(presets, ({ one, many }) => ({
+  user: one(users, {
+    fields: [presets.userId],
+    references: [users.id],
+  }),
+  oscillators: many(oscillators),
+  tags: many(presetsToTags),
+}))
 
-// export const oscillators = pgTable("oscillators", {
-//   id: serial().primaryKey(),
-//   presetId: integer()
-//     .notNull()
-//     .references(() => presets.id, { onDelete: "cascade" }),
-//   index: smallint().notNull(),
-//   volume: real().notNull(), // 0.0 - 1.0 in UI
-//   pan: real().notNull(), // -1.0 to 1.0 in UI
-//   octave: smallint().notNull(), // e.g. -2 .. +2
-//   tune: smallint().notNull(), // fine tune in cents
-//   type: text({ enum: ["sine", "square", "sawtooth", "triangle"] }).notNull(),
-//   ampAttack: real().notNull(), // seconds
-//   ampDecay: real().notNull(), // seconds
-//   ampSustain: real().notNull(), // 0.0 - 1.0
-//   ampRelease: real().notNull(), // seconds
-// })
+export const oscillators = pgTable("oscillators", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  presetId: integer("preset_id")
+    .notNull()
+    .references(() => presets.id, { onDelete: "cascade" }),
+  index: smallint("index").notNull(),
+  volume: real("volume").notNull(),
+  pan: real("pan").notNull(),
+  octave: smallint("octave").notNull(),
+  tune: smallint("tune").notNull(),
+  waveform: text("waveform", {
+    enum: ["sine", "square", "sawtooth", "triangle"],
+  }).notNull(),
+  ampAttack: real("amp_attack").notNull(),
+  ampDecay: real("amp_decay").notNull(),
+  ampSustain: real("amp_sustain").notNull(),
+  ampRelease: real("amp_release").notNull(),
+  filterAttack: real("filter_attack").notNull(),
+  filterDecay: real("filter_decay").notNull(),
+  filterSustain: real("filter_sustain").notNull(),
+  filterRelease: real("filter_release").notNull(),
+})
 
-// export const oscillatorsRelations = relations(oscillators, ({ one }) => ({
-//   preset: one(presets, {
-//     fields: [oscillators.presetId],
-//     references: [presets.id],
-//   }),
-// }))
+export const oscillatorsRelations = relations(oscillators, ({ one }) => ({
+  preset: one(presets, {
+    fields: [oscillators.presetId],
+    references: [presets.id],
+  }),
+}))
 
-// export const tags = pgTable("tags", {
-//   id: serial().primaryKey(),
-//   userId: integer()
-//     .notNull()
-//     .references(() => users.id, { onDelete: "cascade" }),
-//   name: varchar({ length: 50 }).notNull(),
-//   createdAt: timestamp({ mode: "date" }).defaultNow().notNull(),
-// })
+export const tags = pgTable("tags", {
+  id: integer("id").primaryKey().generatedByDefaultAsIdentity(),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  name: varchar("name", { length: 50 }).notNull(),
+  createdAt: timestamp("created_at", { mode: "date" }).defaultNow().notNull(),
+})
 
-// export const tagsRelations = relations(tags, ({ one, many }) => ({
-//   user: one(users, {
-//     fields: [tags.userId],
-//     references: [users.id],
-//   }),
-//   presets: many(presetsToTags),
-// }))
+export const tagsRelations = relations(tags, ({ one, many }) => ({
+  user: one(users, {
+    fields: [tags.userId],
+    references: [users.id],
+  }),
+  presets: many(presetsToTags),
+}))
 
-// export const presetsToTags = pgTable(
-//   "presets_to_tags",
-//   {
-//     presetId: integer()
-//       .notNull()
-//       .references(() => presets.id, { onDelete: "cascade" }),
+export const presetsToTags = pgTable(
+  "presets_to_tags",
+  {
+    presetId: integer("preset_id")
+      .notNull()
+      .references(() => presets.id, { onDelete: "cascade" }),
 
-//     tagId: integer()
-//       .notNull()
-//       .references(() => tags.id, { onDelete: "cascade" }),
-//   },
-//   (table) => ({
-//     pk: primaryKey({ columns: [table.presetId, table.tagId] }),
-//   })
-// )
+    tagId: integer("tag_id")
+      .notNull()
+      .references(() => tags.id, { onDelete: "cascade" }),
+  },
+  (table) => ({
+    pk: primaryKey({ columns: [table.presetId, table.tagId] }),
+  })
+)
 
-// export const presetsToTagsRelations = relations(presetsToTags, ({ one }) => ({
-//   preset: one(presets, {
-//     fields: [presetsToTags.presetId],
-//     references: [presets.id],
-//   }),
-//   tag: one(tags, {
-//     fields: [presetsToTags.tagId],
-//     references: [tags.id],
-//   }),
-// }))
+export const presetsToTagsRelations = relations(presetsToTags, ({ one }) => ({
+  preset: one(presets, {
+    fields: [presetsToTags.presetId],
+    references: [presets.id],
+  }),
+  tag: one(tags, {
+    fields: [presetsToTags.tagId],
+    references: [tags.id],
+  }),
+}))
