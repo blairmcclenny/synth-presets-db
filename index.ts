29acfb85d4789319cbaedb7aff27afbdaccbc217
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-10-30 11:31:01 -0700

    replace seed with reset

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import "dotenv/config"
 import { users } from "./src/db/schema.ts"
 import { drizzle } from "drizzle-orm/neon-http"
-import { seed } from "drizzle-seed"
+import { reset } from "drizzle-seed"
 
 const db = drizzle(process.env.DATABASE_URL!)
 
 async function main() {
   try {
-    await seed(db, { users })
+    await reset(db, { users })
   } catch (error) {
     console.error(error)
   }
