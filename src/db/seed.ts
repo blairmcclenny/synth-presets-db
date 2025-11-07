125cd9800b46fbd120859bfa95f2de77be473b8f
Blair M McClenny <blairmmcclenny@Mac.lan>
2025-11-07 11:52:20 -0800

    seed db wip

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,35 +1,71 @@
 import db from "./connection"
-import { NewTag, tags } from "./schema"
+import {
+  oscillators as oscillatorsTable,
+  presets as presetsTable,
+  presetsToTags as presetsToTagsTable,
+  tags as tagsTable,
+  users as usersTable,
+} from "./schema"
 
 export async function seed() {
   console.log("Starting database seed...")
 
   try {
     console.log("Clearing existing data...")
-    await db.delete(tags)
+    await db.delete(usersTable)
+    await db.delete(presetsTable)
+    await db.delete(oscillatorsTable)
+    await db.delete(tagsTable)
+    await db.delete(presetsToTagsTable)
 
     console.log("Creating preset tags...")
-    const defaultTags: NewTag[] = [
-      "Arp",
-      "Bass",
-      "Lead",
-      "Pad",
-      "Pluck",
-      "Keys",
-      "Aggressive",
-      "Dreamy",
-      "Dark",
-      "Bright",
-      "Evolving",
-      "Analog",
-      "Simple",
-      "Complex",
-    ].map((tag) => ({
-      name: tag,
-      createdAt: new Date(),
-    }))
 
-    const presetTags = await db.insert(tags).values(defaultTags).returning()
+    const tags = await db
+      .insert(tagsTable)
+      .values(
+        [
+          "Arp",
+          "Bass",
+          "Lead",
+          "Pad",
+          "Pluck",
+          "Keys",
+          "Aggressive",
+          "Dreamy",
+          "Dark",
+          "Bright",
+          "Evolving",
+          "Analog",
+          "Simple",
+          "Complex",
+        ].map((tag) => ({
+          name: tag,
+        }))
+      )
+      .returning()
+
+    // console.log("Create oscillators...")
+
+    // const [oscillatorA] = await db
+    //   .insert(oscillatorsTable)
+    //   .values({
+    //     presetId: 10,
+    //     index: 1,
+    //     volume: 0,
+    //     pan: 20,
+    //     octave: 0,
+    //     tune: 500,
+    //     waveform: "sawtooth",
+    //     ampAttack: 1,
+    //     ampDecay: 1,
+    //     ampSustain: 1,
+    //     ampRelease: 1,
+    //     filterAttack: 1,
+    //     filterDecay: 1,
+    //     filterSustain: 1,
+    //     filterRelease: 1,
+    //   })
+    //   .returning()
   } catch (error) {
     console.error(error)
   }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import db from "./db/connection.ts"
-import { tags } from "./db/schema.ts"
+import {
+  oscillators as oscillatorsTable,
+  tags as tagsTable,
+} from "./db/schema.ts"
 
 async function main() {
   try {
-    const presetTags = await db.select().from(tags)
-    console.log(presetTags)
+    const tags = await db.select().from(tagsTable).orderBy(tagsTable.name)
+    const oscillators = await db.select().from(oscillatorsTable)
+
+    console.log(tags)
+    console.log(oscillators)
   } catch (error) {
     console.error(error)
   }
